d9edbe8180c392ee9e785321968e43a67689c5fc
Fabricio <fa.engeroff1996@gmail.com>
2024-08-27 08:08:42 -0300

    Adicionamos metodo topo do tipo T, que espia o ultimo elemento da pilha se estiver vazio ele retorna null se não ele traz o umtimo elemento da pilha.

--- src/main/java/org/estruturaDeDados/pilha/PilhaTest.java
+++ src/main/java/org/estruturaDeDados/pilha/PilhaTest.java
@@ -5,16 +5,16 @@ import org.estruturaDeDados.contatoVetor.Vetor;
 public class PilhaTest {
 
     public static void main(String[] args) {
-        // Cria um vetor com capacidade para 3 elementos
+
        Pilha<Integer> pilha = new Pilha<Integer>();
 
+       pilha.empilhar(1);
+       pilha.empilhar(2);
 
         System.out.println(pilha);
+
+        System.out.println(pilha.topo());
        System.out.println(pilha.estaVazia());
-
-       pilha.empilhar(1);
-
-        System.out.println(pilha.estaVazia());
 
     }
 }
