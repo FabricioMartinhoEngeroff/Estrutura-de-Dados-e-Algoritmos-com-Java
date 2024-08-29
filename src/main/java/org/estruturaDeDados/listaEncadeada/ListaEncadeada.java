d20d2b14ed2f982ec249c37025a81ee385076a22
Fabricio <fa.engeroff1996@gmail.com>
2024-08-29 07:19:46 -0300

    Adicionei um contador do tamanho da lista encadeada.

--- src/main/java/org/estruturaDeDados/listaEncadeada/ListaEncadeada.java
+++ src/main/java/org/estruturaDeDados/listaEncadeada/ListaEncadeada.java
@@ -3,4 +3,22 @@ package org.estruturaDeDados.listaEncadeada;
 public class ListaEncadeada<T>{
 
     private No<T> incio;
+    private int tamanho;
+
+    public void adiciona(T elemento){
+        No<T> celula = new No<>(elemento);
+        this.incio = celula;
+        this.tamanho++;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" +
+                "incio=" + incio +
+                '}';
+    }
 }
--- src/main/java/org/estruturaDeDados/listaEncadeada/ListaEncadeadaTest.java
+++ src/main/java/org/estruturaDeDados/listaEncadeada/ListaEncadeadaTest.java
@@ -1,2 +1,15 @@
-package org.estruturaDeDados.listaEncadeada;public class ListaEncadeadaTest {
+package org.estruturaDeDados.listaEncadeada;
+
+public class ListaEncadeadaTest {
+
+    public static void main(String[] args) {
+
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adiciona(1);
+
+        System.out.println(lista.getTamanho());
+        System.out.println(lista);
+
+    }
 }
